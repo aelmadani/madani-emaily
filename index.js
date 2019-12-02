0968ae481f0f96b671f451644fb969bfbdc39eb3
Ahmad Madani <ahmadmadani@Ahmads-MacBook-Pro.local>
2019-12-02 21:16:00 +0100

    auth and env var done

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+require("./models/User");
+require("./services/passport");
+const authRoutes = require("./routes/authRoutes");
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there hahahahaha" });
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
-const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
+
+// app.get("/", (req, res) => {
+//   res.send({ hi: "there hahahahaha" });
+// });
+
+const PORT = process.env.PORT || 5050;
 app.listen(PORT);
---
+++ notes.md
@@ -0,0 +1,16 @@
+Authentication: OAuth with help by passport js
+Deployment: Azure Web Apps
+DB: MongoDB
+
+Dependencies:
+npm install --save nodemon
+npm install --save express
+npm install --save passport passport-google-oauth20
+npm install --save mongoose
+npm install --save cookie-session
+
+Steps:
+1- set server up
+2- deploy to github and azure
+3- setup Oauth
+4- setup MongoDB and create Schemas
